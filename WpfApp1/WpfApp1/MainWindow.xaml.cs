e59dfcf895a999882e0f808285c6084d0b77d399
Vi1317 <leejiah1317@gmail.com>
2019-03-06 21:17:16 +0700

    2lab

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        double A=0, B=0, Res = 0;
+
+        private void mns_Click(object sender, RoutedEventArgs e)
+        {
+            A = double.Parse(a.Text);
+            B = double.Parse(b.Text);
+            Res = A - B;
+            res.Text = Res.ToString();
+        }
+
+        private void umn_Click(object sender, RoutedEventArgs e)
+        {
+            A = double.Parse(a.Text);
+            B = double.Parse(b.Text);
+            Res = A * B;
+            res.Text = Res.ToString();
+        }
+
+        private void del_Click(object sender, RoutedEventArgs e)
+        {
+            A = double.Parse(a.Text);
+            B = double.Parse(b.Text);
+            Res = A / B;
+            res.Text = Res.ToString();
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void pls_Click(object sender, RoutedEventArgs e)
+        {
+            A = double.Parse(a.Text);
+            B = double.Parse(b.Text);
+            Res = A + B;
+            res.Text = Res.ToString();
+        }
+    }
+}
